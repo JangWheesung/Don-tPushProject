bd7ec4596d5d5ff7b940cd92c2a39cc7670cf640
JangWheesung <gameplay1qaz@gmail.com>
2023-12-01 00:49:52 +0900

    대시 감지

--- Assets/01.Scripts/Combat/DealDamageOnConcat.cs
+++ Assets/01.Scripts/Combat/DealDamageOnConcat.cs
@@ -1,17 +1,42 @@
+using System;
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class DealDamageOnContact : NetworkBehaviour
 {
+    private PlayerMovement playerMovement;
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] GameObject test1;
+    [SerializeField] GameObject test2;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Slickback(Vector2 vec)
+    {
+        Instantiate(test2, transform.position, Quaternion.identity);
+        StartCoroutine(playerMovement.NuckBack(vec));
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.attachedRigidbody == null) return;
-        Debug.Log("NotReturn");
+
         if (collision.attachedRigidbody.TryGetComponent<Health>(out Health health))
         {
-            Debug.Log("TakeID");
             health.TakeID(OwnerClientId);
         }
-    }
 
+        if (playerMovement.isDash && collision.attachedRigidbody.TryGetComponent<DealDamageOnContact>
+            (out DealDamageOnContact dealDamageOnContact))
+        {
+            Instantiate(test1, dealDamageOnContact.transform.position, Quaternion.identity);
+            dealDamageOnContact.Slickback(Vector2.left);
+        }
+    }
 }
--- Assets/01.Scripts/Player/PlayerMovement.cs
+++ Assets/01.Scripts/Player/PlayerMovement.cs
@@ -2,19 +2,24 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private InputReader _inputReader;
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _dashSpeed;
+    [SerializeField] private float _nuckbackDelay;
     [SerializeField] private float maxVelocity;
-
+    
     [HideInInspector] public Rigidbody2D _rigidbody2D;
+    [HideInInspector] public Vector2 dashVec;
     private PlayerAnimation _playerAnimation;
     private Vector2 _movementInput;
+    [SerializeField] private GameObject dast;
 
-    public bool isDash;
+    public bool isDash { get; private set; }
+    public bool isBack { get; private set; }
 
     private void Awake()
     {
@@ -43,17 +48,19 @@ public class PlayerMovement : NetworkBehaviour
 
     private void HandleDash()
     {
-        if (_rigidbody2D.velocity == Vector2.zero || isDash) return;
+        if (_rigidbody2D.velocity == Vector2.zero || isDash || isBack) return;
 
         isDash = true;
-        _rigidbody2D.velocity = _movementInput * _dashSpeed;
+        dashVec = _rigidbody2D.velocity.normalized;
+        _rigidbody2D.velocity = dashVec * _dashSpeed;
         StartCoroutine(DashColldown());
     }
 
     IEnumerator DashColldown()
     {
-        yield return new WaitForSeconds(0.4f);
-        _rigidbody2D.velocity = Vector2.zero;
+        yield return new WaitForSeconds(0.2f);
+        dashVec = Vector2.zero;
+        _rigidbody2D.velocity = dashVec;
         isDash = false;
     }
 
@@ -68,7 +75,7 @@ public class PlayerMovement : NetworkBehaviour
     private void Velocity()
     {
         if (!IsOwner) return;
-        if (isDash) return;
+        if (isDash || isBack) return;
 
         Vector2 desiredVelocity = _movementInput.normalized * _movementSpeed;
 
@@ -95,5 +102,17 @@ public class PlayerMovement : NetworkBehaviour
             float symbol = _rigidbody2D.velocity.y > 0 ? -1f : 1f;
             _rigidbody2D.velocity += new Vector2(0, symbol * _movementSpeed / 2);
         }
+    }
+
+    public IEnumerator NuckBack(Vector2 vec)
+    {
+        isBack = true;
+        isDash = false;
+        StopCoroutine(DashColldown());
+        //Instantiate(dast, vec, Quaternion.identity);
+        _rigidbody2D.velocity = vec * _dashSpeed;
+        yield return new WaitForSeconds(_nuckbackDelay);
+        _rigidbody2D.velocity = Vector2.zero;
+        isBack = false;
     }
 }
